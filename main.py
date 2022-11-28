9627e18a964d1bef796d98bfbae13166e17e1442
Jonah Greening <116300133+JonahJG@users.noreply.github.com>
2022-11-28 12:23:57 -0330

    Add files via upload
    Graph for showing the amount of claims over the course of a year. 11-28-2022

---
+++ main.py
@@ -0,0 +1,27 @@
+# Program to create a graph to show the amounts of claims per month
+# Author: Jonah Greening              Date:
+
+import matplotlib.pyplot as plt
+
+Amt_List = []
+
+List_Length = 12
+
+for Amt in range(List_Length):
+    Num = int(input("Enter the claims amount: "))
+    Amt_List.append(Num)
+
+
+x_axis = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+y_axis = Amt_List
+
+plt.title("Total Amount of Claims ($)")
+plt.scatter(x_axis, y_axis, color="red", marker="x", label="Claims Amount")
+
+plt.xlabel("Time (months)")
+plt.ylabel("Price (dollars)")
+
+plt.grid(True)
+plt.legend()
+
+plt.show()
